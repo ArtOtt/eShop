a75a638ed078c0da599936683eb93a7b1e511915
a.o <arthur.ott91@web.de>
2023-03-24 11:45:29 +0100

    React Routing

--- src/Header.js
+++ src/Header.js
@@ -3,16 +3,18 @@ import "./Header.css";
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StoreIcon from '@mui/icons-material/Store';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-          
-      <div className="header__logo">
-        <StoreIcon className="header__logoImage" fontSize="large"/>
-        <h2 className="header__logoTitle">eShop</h2>
-      </div>
-          
+      <Link to="/" style={{textDecoration:"none"}}>
+        <div className="header__logo">
+          <StoreIcon className="header__logoImage" fontSize="large"/>
+          <h2 className="header__logoTitle">eShop</h2>
+        </div>   
+      </Link>
+      
             
 
       <div className="header__search">
@@ -31,11 +33,14 @@ function Header() {
           <span className="nav__itemLineOne">Your</span>
           <span className="nav__itemLineTwo">Shop</span>
         </div>
+        <Link to="/checkout" style={{textDecoration:"none"}}>
+          <div className="nav__itemBasket">
+            <ShoppingBasketIcon />
+            <span className="nav__itemLineTwo nav__basketCount nav_basketCount">0</span>
+          </div>
+        </Link>
         
-        <div className="nav__itemBasket">
-          <ShoppingBasketIcon />
-          <span className="nav__itemLineTwo nav__basketCount nav_basketCount">0</span>
-        </div>
+        
         
       </div>
     </div>
