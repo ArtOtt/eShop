89f78d56f9e7d907863a24856d86b3868815574c
a.o <arthur.ott91@web.de>
2023-03-24 10:52:45 +0100

    using props for Products

--- src/Home.js
+++ src/Home.js
@@ -11,18 +11,52 @@ function Home() {
                 className="home__image" />
 
                 <div className="home__row">
-                    <Product/>
-                    <Product/>
+                    <Product
+                    id="1234"
+                    title=" Amazon Basics - Laptop-Trolley, mit schnell laufenden Rollen und leicht zugänglicher Vordertasche - geeignet für Laptops bis 16 Zoll (40 cm)"
+                    price={12.99}
+                    rating={2}
+                    image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81e-8Or4d7L._AC_SL1500_.jpg" />
+                    <Product
+                    id="6234"
+                    title="Amazon Basics - Hartplatten-Klemmbrett - 10er-Packung "
+                    price={12.99}
+                    rating={4}
+                    image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81jki4Or7tL._AC_SL1500_.jpg" />
+                   
                 </div>
 
                 <div className="home__row">
-                    <Product/>
-                    <Product/>
-                    <Product/>
+                    <Product
+                     id="2234"
+                     title="Amazon Basics - Gaming-Mauspad "
+                     price={13.99}
+                     rating={4}
+                     image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71o2ghrFt3L._AC_SL1500_.jpg" />
+                    <Product
+                     id="3234"
+                     title="Amazon Basics – LR44 Alkaline-Knopfzelle, 6 stück  "
+                     price={4.99}
+                     rating={5}
+                     image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71PGQqyI4UL._AC_SL1500_.jpg" />
+                    
+                    <Product
+                    id="4234"
+                    title="Amazon Basics - PC-Lautsprecher mit dynamischem Sound, USB-Betrieb, Schwarz, 1 Paar "
+                    price={14.99}
+                    rating={4}
+                    image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81IEOd-7PxL._AC_SL1500_.jpg" />
+                   
                 </div>
 
                 <div className="home__row">
-                    <Product/>
+                    <Product
+                    id="5234"
+                    title="Amazon Basics Aktenvernichter, 7-8 Blatt, Streifenschnitt, CD-Schredder "
+                    price={33.99}
+                    rating={4}
+                    image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71nU5j6fH-L._AC_SL1500_.jpg" />
+                   
                 </div>
             </div>   
         </div> 
