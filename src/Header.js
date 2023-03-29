3af4069813c02bc8348eb9716e100dce95a0461e
a.o <arthur.ott91@web.de>
2023-03-29 16:45:02 +0200

    Add to Basket 1/2

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,10 @@ import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StoreIcon from '@mui/icons-material/Store';
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  const [{basket}, dispatch] = useStateValue();
   return (
     <div className="header">
       <Link to="/" style={{textDecoration:"none"}}>
@@ -36,7 +38,7 @@ function Header() {
         <Link to="/checkout" style={{textDecoration:"none"}}>
           <div className="nav__itemBasket">
             <ShoppingBasketIcon />
-            <span className="nav__itemLineTwo nav__basketCount nav_basketCount">0</span>
+            <span className="nav__itemLineTwo nav__basketCount nav_basketCount">{basket.length}</span>
           </div>
         </Link>
         
